928e7d562623420eb9bc74381f2fd3c902f9679d
Maicon_dev <maicon.pontes.dev@gmail.com>
2026-05-23 17:56:07 -0300

    ajustes na aba adicinar cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.clientes, name="clientes")
+    path('', views.clientes, name="clientes"),
+    path('Atualiza_cliente/', views.att_clientes, name="atualiza_cliente")
 ]
